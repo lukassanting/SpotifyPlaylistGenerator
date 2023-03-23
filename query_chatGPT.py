d334219d733ff3070ec2ad9f6feec83d8682a072
Lukas Santing <lukas.santing@gmail.com>
2023-03-23 10:57:32 +0100

    Fixed some issues in prompting

--- query_chatGPT.py
+++ query_chatGPT.py
@@ -8,25 +8,23 @@ def generate_playlist_chatGPT():
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
     # inputs
-    styles = input(
-        "Give a list of songs / genres / artists to use a base (in the form 'Twenty One Pilots and ... and ...': ")
+    styles = input("Give a list of songs / genres / artists to use a base (in the form 'Twenty One Pilots and ... and ...': ")
     number_of_songs = input("How many songs should the playlist contain? ")
-    limit = input(
-        "Should there be a limit to the number of songs similar to the prompt? (Y/N) ")
+    limit = input("Should there be a limit to the number of songs similar to the prompt? (Y/N) ")
     if limit == "Y":
         limited_prompt = input("How many should it be limited to? ")
 
     # create prompt
-    new_prompt = f"Create a playlist of at least 15 songs similar to {styles}"
+    new_prompt = f"Create a playlist of at least {number_of_songs} songs similar to {styles}"
     if limit == "Y":
-        new_prompt = new_prompt + f", that does not contain more than 5 songs by them"
+        new_prompt = new_prompt + f", that does not contain more than {limited_prompt} songs by them"
     new_prompt = new_prompt + ", and give it a title in the form 'Title: <title>'"
 
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=new_prompt,
         temperature=0.6,
-        max_tokens=int(number_of_songs) * 100
+        max_tokens=int(number_of_songs) * 50
     )
 
     text_response = response.choices[0].text
